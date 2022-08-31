3ea3a34f4987835d92aa5f7e8a990f44d764f011
EldosHD <Eldos.sonofelax@gmail.com>
2022-09-01 00:43:49 +0200

    added translator page

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This is the code for my amazing website."""
 
+import re
 from flask import Flask, render_template, redirect, url_for, session, request
 from flask import flash
 from datetime import timedelta
 import argparse
+import owo
 
 app = Flask(__name__)
 app.permanent_session_lifetime = timedelta(days=5)
@@ -23,6 +25,12 @@ def about():
 @app.route('/projects/')
 def projects():
     return render_template('projects.html')
+
+@app.route('/owo/')
+@app.route('/uwu/')
+@app.route('/translator/')
+def translator():
+    return render_template('translator.html')
 
 
 if __name__ == '__main__':

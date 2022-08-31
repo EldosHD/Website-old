aa0df50870147376d778a93d52a5459e4227ec20
EldosHD <Eldos.sonofelax@gmail.com>
2022-09-01 00:23:03 +0200

    added an argument parser to the main script.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 from flask import flash
 from datetime import timedelta
+import argparse
 
 app = Flask(__name__)
 app.permanent_session_lifetime = timedelta(days=5)
@@ -19,5 +20,19 @@ def home():
 def about():
     return render_template('about.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--debug',action='store_true' ,default=False ,help='Enables debug mode for the flask')
+    parser.add_argument('-p','--port',default=80, type=int ,help='Specify the port for the webserver')
+    parser.add_argument('--test',action='store_true' ,default=False ,help='Sets debug mode to true and the port to 5000')
+
+    args = parser.parse_args()
+    
+    if args.test == True:
+        args.debug = True
+        args.port = 5000
+
+    app.run(debug=args.debug, port=args.port)
